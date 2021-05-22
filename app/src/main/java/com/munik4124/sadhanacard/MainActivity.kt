9cf89e54eb6094ada891699aade70f387f75a580
yashsinghcs <yash.singh_cs18@gla.ac.in>
2021-05-22 23:54:59 +0530

    init commit

---
+++ app/src/main/java/com/munik4124/sadhanacard/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.munik4124.sadhanacard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
