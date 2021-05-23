a14d24ca5f4ece4861503a72fd168c77ceeb9585
balaram714 <nityanandasamanta96@gmail.com>
2021-05-24 01:26:09 +0530

    Login page for devotees has been created.

--- app/src/main/java/com/munik4124/sadhanacard/ChoiceAdminVsDevotees.kt
+++ app/src/main/java/com/munik4124/sadhanacard/ChoiceAdminVsDevotees.kt
@@ -1,14 +1,22 @@
 package com.munik4124.sadhanacard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.ImageView
 
 class ChoiceAdminVsDevotees : AppCompatActivity() {
+    private lateinit var devotees_logo : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choice_admin_vs_devotees)
 
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        devotees_logo = findViewById(R.id.devotees_icon)
+        devotees_logo.setOnClickListener{
+           startActivity(Intent(applicationContext,DevoteesLogin::class.java))
+        }
     }
 }
