1ec4b47a54aa29fc5e1fab83bcf8ea206d39d7ef
balaram714 <nityanandasamanta96@gmail.com>
2021-06-10 02:06:12 +0530

    Additional details fragmaint layout complited

--- app/src/main/java/com/munik4124/sadhanacard/fragments_admin_userdetails/AdditionalDetailsFragment.kt
+++ app/src/main/java/com/munik4124/sadhanacard/fragments_admin_userdetails/AdditionalDetailsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import com.munik4124.sadhanacard.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -31,7 +33,29 @@ class AdditionalDetailsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_additional_details, container, false)
+        var binding = inflater.inflate(R.layout.fragment_additional_details, container, false)
+
+        val interested : AutoCompleteTextView = binding.findViewById(R.id.interested)
+        val languages1 = resources.getStringArray(R.array.choise)
+        val arrayAdapter1 = ArrayAdapter(requireContext(), R.layout.blood_group, languages1)
+       interested.setAdapter(arrayAdapter1)
+
+        val aware_sadhnaCard : AutoCompleteTextView = binding.findViewById(R.id.aware_sadhnaCard)
+        val languages2 = resources.getStringArray(R.array.choise)
+        val arrayAdapter2 = ArrayAdapter(requireContext(), R.layout.blood_group, languages2)
+        aware_sadhnaCard.setAdapter(arrayAdapter2)
+
+        val badHabbits : AutoCompleteTextView = binding.findViewById(R.id.badHabbits)
+        val languages3 = resources.getStringArray(R.array.choise)
+        val arrayAdapter3 = ArrayAdapter(requireContext(), R.layout.blood_group, languages3)
+        badHabbits.setAdapter(arrayAdapter3)
+
+        val visitedTemple : AutoCompleteTextView = binding.findViewById(R.id.visitedTemple)
+        val languages4 = resources.getStringArray(R.array.choise)
+        val arrayAdapter4 = ArrayAdapter(requireContext(), R.layout.blood_group, languages4)
+        visitedTemple.setAdapter(arrayAdapter4)
+
+        return binding
     }
 
 
